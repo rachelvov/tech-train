0155a0a44bf4ab58a2443cc37fc3cf1a2fe6c835
rachelvov <rachelvov@gmail.com>
2024-01-02 01:38:36 +0000

    added functions and more steps

--- notebooks/tech_train_functions.py
+++ notebooks/tech_train_functions.py
@@ -1,5 +1,35 @@
 import openai
 
+import json
+import os
+
+import requests
+
+
+class TA4HPredictor:
+    def __init__(self):
+        self.endpoint = os.getenv("TA4H_ENDPOINT")
+
+    def predict_ta4h(self, texts):
+        """
+        call TA4H API to get entities and relations
+        This assumes a ta4h synchronous endpoint. This currently only exists on the onprem container,
+        at {base_url}/text/analytics/v3.1/entities/health.
+
+        :param text:
+        :return: list of entities and relations
+       """
+        payload = json.dumps({
+            "documents": [{"id": idx, "text": text} for idx, text in enumerate(texts)],
+            "output_format": "ayalon"
+        })
+        headers = {'Content-Type': 'application/json'}
+        response = requests.request("POST", self.endpoint, headers=headers, data=payload)
+        response = json.loads(response.text)
+        entities = [r["entities"] for r in response['documents']]
+        return entities
+    
+
 def call_open_ai(system_message, text, engine="gpt-35-turbo", examples=[]):
     message_text = [{"role": "system", "content": system_message}]
     for example in examples:
@@ -21,3 +51,16 @@ def call_open_ai(system_message, text, engine="gpt-35-turbo", examples=[]):
     else:
         print(completion)
         return "none"
+    
+
+def is_similar(gold_answer, gold_answer_idx, answer):
+        if len(answer) == 1:
+            return 1 if gold_answer_idx  == answer else 0
+        gold_answer = gold_answer.lower()
+        answer = answer.lower()
+        return 1 if gold_answer in  answer or answer in gold_answer else 0
+
+def create_example_CoT(system_message_explainer, question, options, answer):   
+    question = "Question: "+question+" "+ str(options)+"."
+    explained_answer = call_open_ai(system_message_explainer, question+" Correct answer: "+ answer)    
+    return question, explained_answer
