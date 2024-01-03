3d043877e15a2623f1a7320be26a3dd106a060c9
rachelvov <rachelvov@gmail.com>
2024-01-03 00:02:58 +0000

    added option to connect with my key

--- notebooks/tech_train_functions.py
+++ notebooks/tech_train_functions.py
@@ -30,27 +30,48 @@ class TA4HPredictor:
         return entities
     
 
-def call_open_ai(system_message, text, engine="gpt-35-turbo", examples=[]):
+def call_open_ai(system_message, text, engine="gpt-35-turbo", examples=[], client=None, temprature=0):
     message_text = [{"role": "system", "content": system_message}]
     for example in examples:
         message_text.append({"role": "user", "content": example[0]})
         message_text.append({"role": "assistant", "content": example[1]})
     message_text.append({"role": "user", "content": text})
-    completion = openai.ChatCompletion.create(
-        engine=engine,
+    if client==None: #use azure openAI
+        completion = openai.ChatCompletion.create(
+            engine=engine,
+            messages=message_text,
+            temperature=temprature,
+            max_tokens=800,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+
+        if "content" in completion["choices"][0]["message"]:
+                return completion["choices"][0]["message"]["content"]
+        else:
+                print(completion)
+                return "none"
+    else: #use openAI studio
+        if engine=="gpt-35-turbo":
+            model="gpt-3.5-turbo"
+        elif engine=="gpt4":
+            model="gpt-4"
+        else:
+             model=engine
+        completion = client.chat.completions.create(
+        model=model,
         messages=message_text,
-        temperature=0,
+        temperature=temprature,
         max_tokens=800,
-        top_p=0.95,
+        top_p=1,
         frequency_penalty=0,
-        presence_penalty=0,
-        stop=None
-    )
-    if "content" in completion["choices"][0]["message"]:
-        return completion["choices"][0]["message"]["content"]
-    else:
-        print(completion)
-        return "none"
+        presence_penalty=0
+        )
+        
+        return completion.choices[0].message.content
+     
     
 
 def is_similar(gold_answer, gold_answer_idx, answer):
@@ -60,7 +81,7 @@ def is_similar(gold_answer, gold_answer_idx, answer):
         answer = answer.lower()
         return 1 if gold_answer in  answer or answer in gold_answer else 0
 
-def create_example_CoT(system_message_explainer, question, options, answer):   
+def create_example_CoT(system_message_explainer, question, options, answer, client=None):   
     question = "Question: "+question+" "+ str(options)+"."
-    explained_answer = call_open_ai(system_message_explainer, question+" Correct answer: "+ answer)    
+    explained_answer = call_open_ai(system_message_explainer, question+" Correct answer: "+ answer, client=client)    
     return question, explained_answer
